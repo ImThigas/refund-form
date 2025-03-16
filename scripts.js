565faed6778dbd6804686bb38f5b1314c48e9ac2
Thiago Carvalho <thiagoomaciel@hotmail.com>
2025-03-16 14:46:37 -0300

    feat: created function to add a new expense

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 const amount = document.querySelector("#amount")
+const expense = document.querySelector("#expense")
+const category = document.querySelector("#category")
+const form = document.querySelector("form")
 
 amount.oninput = () =>{
     let value = amount.value.replace(/\D/g, "")
@@ -15,4 +18,27 @@ function formatCurrencyBRL (value){
     })
 
     return value
+}
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault()
+
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+    expenseAdd(newExpense)
+})
+
+function expenseAdd(newExpense){
+    try{
+        
+    }catch(error){
+        alert("Não foi possível atualizar a lista de despesas.")
+        console.log(error)
+    }
 }
